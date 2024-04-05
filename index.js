f5288847668a56350b9fa8384ccc8e467531fc8d
tigrazone <tigrazone@gmail.com>
2024-04-05 10:45:46 +0300

    add git sites, key, push key

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const request = require('request');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const collection = 'sites'
+
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
@@ -20,48 +22,42 @@ app.use(express.urlencoded({ extended: true }))
 // app.use(express.static('public', options))
 // #############################################################################
 
-/*
 // Create or Update an item
-app.post('/:col/:key', async (req, res) => {
+app.post('/:key', async (req, res) => {
   console.log(req.body)
 
-  const col = req.params.col
   const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
+  const item = await db.collection(collection).set(key, req.body)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
 })
 
+/*
 // Delete an item
 app.delete('/:col/:key', async (req, res) => {
   const col = req.params.col
   const key = req.params.key
   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).delete(key)
+  const item = await db.collection(collection).delete(key)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
 })
+*/
 
 // Get a single item
-app.get('/:col/:key', async (req, res) => {
-  const col = req.params.col
+app.get('/:key', async (req, res) => {
   const key = req.params.key
-  console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).get(key)
+  const item = await db.collection(collection).get(key)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
 })
 
 // Get a full listing
-app.get('/:col', async (req, res) => {
-  const col = req.params.col
-  console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-  const items = await db.collection(col).list()
+app.get('/sites', async (req, res) => {
+  const items = await db.collection(collection).list()
   console.log(JSON.stringify(items, null, 2))
   res.json(items).end()
 })
-*/
 
 // ping sites
 app.get('/check_url', async (req, res) => {  
